17facee4a64e53712d1cc1a01c8964295bb6f85f
arifur <arif@newgen-bd.com>
2018-09-20 17:45:13 +0600

    ##initcommit

---
+++ src/main/java/org/apps/springular/SpringularApplication.java
@@ -0,0 +1,12 @@
+package org.apps.springular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringularApplication.class, args);
+	}
+}
